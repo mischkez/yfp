f048ca6ede5f3593b2805c5f979322bca9b08dc6
Milos Jankovic <jankovic.m87@gmail.com>
2024-01-21 20:23:36 +0100

    show comments etc...

--- larablog/app/Http/Controllers/HomeController.php
+++ larablog/app/Http/Controllers/HomeController.php
@@ -22,15 +22,26 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function index(Request  $request)
     {
         $posts = Post::orderBy('created_at','desc')->paginate(25);
+
+        if ($request->query('format') === 'json') { // example how we can return JSON responses that is paginated
+            return $posts;
+        }
 
         return view('home', ['posts' => $posts]);
     }
 
-    public function show(Post $post)
+    public function show(Request $request, Post $post)
     {
+        $post = $post->load(['author', 'comments']);
+
+        // this is just an example, there are better ways to do this
+        if ($request->query('format') === 'json') {
+            return $post;
+        }
+
         return view('posts.show', [
             'post' => $post
         ]);
--- larablog/app/Models/Comment.php
+++ larablog/app/Models/Comment.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+
+    protected $with = ['author']; // eager load author, meaning that when we fetch a comment, we also fetch the author
+
+    public function author()
+    {
+        return $this->belongsTo(\App\Models\User::class, 'author_id');
+    }
+
+    public function post()
+    {
+        return $this->belongsTo(\App\Models\Post::class);
+    }
 }
+
